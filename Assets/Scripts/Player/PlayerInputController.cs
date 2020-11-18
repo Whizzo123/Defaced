b810ecb9b2987d81e27e2e251bbc419fda3ad5d7
Scobmyster <jaa.scobbie@gmail.com>
2020-11-18 12:39:06 +0000

    Pushable Objects

--- Assets/Scripts/Player/PlayerInputController.cs
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -9,12 +9,15 @@ public class PlayerInputController : MonoBehaviour
 
     public float HorizontalThreshold;
     private PlayerMovementController mv_controller;
-
+    private Rigidbody2D playerBody;
+    public bool hasStrength;
+    private GameObject objectBeingPushed;
 
 
     void Start()
     {
         mv_controller = GetComponent<PlayerMovementController>();
+        hasStrength = true;
     }
 
     void Update()
@@ -41,6 +44,35 @@ public class PlayerInputController : MonoBehaviour
         if(this.transform.position.y < -20f)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(hasStrength)
+        {
+            if(other.gameObject.tag == "Pushable")
+            {
+                if (other.gameObject.GetComponent<Rigidbody2D>())
+                {
+                    other.gameObject.GetComponent<Rigidbody2D>().WakeUp();
+                    objectBeingPushed = other.gameObject;
+                }
+                else
+                    Debug.LogError("You've forgotten to attach a rigidbody to this!!");
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Pushable")
+        {
+            if(other.gameObject == objectBeingPushed)
+            {
+                other.gameObject.GetComponent<Rigidbody2D>().Sleep();
+                objectBeingPushed = null;
+            }
         }
     }
 
