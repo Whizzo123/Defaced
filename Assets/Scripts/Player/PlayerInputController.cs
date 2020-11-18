3215e0caf281324a9681cea62766ff4e3f389ec3
Scobmyster <jaa.scobbie@gmail.com>
2020-11-18 10:59:32 +0000

    Basic Player Controller
    Player can move and jump and has ability to unlock double jump.

---
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerInputController : MonoBehaviour
+{
+
+
+    public float HorizontalThreshold;
+    private PlayerMovementController mv_controller;
+
+    void Start()
+    {
+        mv_controller = GetComponent<PlayerMovementController>();
+    }
+
+    void Update()
+    {
+        if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) > HorizontalThreshold)
+        {
+            //Move player
+            mv_controller.Move(Input.GetAxisRaw("Horizontal"));
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Player jump
+            mv_controller.Jump();
+        }
+
+        if(this.transform.position.y < -20f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+
+    private Rigidbody2D playerBody;
+    public float moveSpeed;
+    public float cappedVelocity;
+    public float jumpPower;
+    public float frictionTimeDivider;
+    private PlayerInputController input_Controller;
+    private int jumpCounter;
+    public int jumpLimit;
+    private bool onGround;
+
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        input_Controller = GetComponent<PlayerInputController>();
+    }
+
+    public void Move(float horizontalInput)
+    {
+        Vector2 moveVelocity = new Vector2(horizontalInput * (moveSpeed * Time.deltaTime), 0);
+        if(Mathf.Abs(playerBody.velocity.x) < cappedVelocity)
+        {
+            playerBody.velocity += moveVelocity;
+        }
+    }
+
+    void Update()
+    {
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) < input_Controller.HorizontalThreshold)
+        {
+            if (Mathf.Abs(playerBody.velocity.x) > .1f)
+            {
+                float friction = Mathf.Lerp(playerBody.velocity.x, 0, Time.deltaTime / frictionTimeDivider);
+                playerBody.velocity = new Vector2(friction, playerBody.velocity.y);
+            }
+            else
+            {
+                playerBody.velocity = new Vector2(0, playerBody.velocity.y);
+            }
+        }
+    }
+
+    public void Jump()
+    {
+        if (jumpCounter < jumpLimit)
+        {
+            Vector2 jumpVelocity = new Vector2(0, jumpPower);
+            playerBody.velocity += jumpVelocity;
+            jumpCounter++;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == ("Ground"))
+        {
+            onGround = true;
+            jumpCounter = 0;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        onGround = false;
+    }
+
+}
