ec6d7d2da1f73c48b2239edb0e2ca87a91b2bd13
Scobmyster <jaa.scobbie@gmail.com>
2020-11-18 22:21:11 +0000

    Update Projectile Stuff

---
+++ Assets/Scripts/Physics/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Projectile : MonoBehaviour
+{
+
+    public float timeAlive;
+    private float timeAliveCounter;
+
+    // Use this for initialization
+    void Start()
+    {
+        timeAliveCounter = timeAlive;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAlive -= Time.deltaTime;
+    }
+}
