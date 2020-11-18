56ab0ee4f020e25744a6e9a683f930c1b87bee05
Scobmyster <jaa.scobbie@gmail.com>
2020-11-18 20:44:28 +0000

    Update MaskWheel

--- Assets/Scripts/UI/MaskWheelComponent.cs
+++ Assets/Scripts/UI/MaskWheelComponent.cs
@@ -24,21 +24,30 @@ public class MaskWheelComponent : MonoBehaviour, IPointerEnterHandler, IPointerE
     public void SetUpSprites(MaskWheelSprite sprites)
     {
         foreground.sprite = sprites.foreground;
+        Color foreColor = foreground.color;
+        foreColor.a = 1f;
+        foreground.color = foreColor;
         background.sprite = sprites.background;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Color tempColor = background.color;
-        tempColor.a = .5f;
-        background.color = tempColor;
-        FindObjectOfType<MaskWheel>().SetSegmentSelected(this);
+        if (equippedMask != MASKS.NONE)
+        {
+            Color tempColor = background.color;
+            tempColor.a = .5f;
+            background.color = tempColor;
+            FindObjectOfType<MaskWheel>().SetSegmentSelected(this);
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        FindObjectOfType<MaskWheel>().SetSegmentDeselected();
-        background.color = Color.white;
+        if (equippedMask != MASKS.NONE)
+        {
+            FindObjectOfType<MaskWheel>().SetSegmentDeselected();
+            background.color = Color.white;
+        }
     }
 
 }
