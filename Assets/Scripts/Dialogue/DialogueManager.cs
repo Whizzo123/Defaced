80a9fb862d393e91aa09ab0e417505f452624adb
RenDeveloper <duncansrendall@gmail.com>
2020-11-18 11:24:42 +0000

    Intial file creation

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> setences;
+
+    void Start()
+    {
+        setences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("starting convo with " + dialogue.name);
+    }
+}
