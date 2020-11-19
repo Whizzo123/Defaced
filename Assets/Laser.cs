d06011a5eeec5a08443f4dfc15a29410ee22384e
Scobmyster <jaa.scobbie@gmail.com>
2020-11-19 07:15:23 +0000

    DeathPlatform

---
+++ Assets/Laser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    public float laserFireRate;
+    private float laserFireRateCounter;
+
+    void Update()
+    {
+        if(laserFireRateCounter <= 0)
+        {
+            laserFireRateCounter = laserFireRate;
+            Plane zeroPlane = new Plane(Vector3.down, Vector3.zero);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distance;
+
+            if(zeroPlane.Raycast(ray, out distance))
+            {
+                Vector2 outputPosition = ray.origin + ray.direction * distance;
+                GetComponent<LineRenderer>().SetPosition(0, this.transform.position);
+                GetComponent<LineRenderer>().SetPosition(1, outputPosition);
+                Debug.DrawLine(this.transform.position, outputPosition);
+
+            }
+        }
+        laserFireRateCounter -= Time.deltaTime;
+    }
+
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject deathPlatformPrefab;
+    public GameObject spawnPoint;
+
+    public float spawnRate;
+    private float spawnRateCounter;
+
+
+    void Update()
+    {
+        if (spawnRateCounter <= 0)
+        {
+            spawnRateCounter = spawnRate;
+            GameObject go = (GameObject)Instantiate(deathPlatformPrefab, spawnPoint.transform.position, Quaternion.identity);
+            go.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1);
+        }
+        spawnRateCounter -= Time.deltaTime;
+    }
+
+}
