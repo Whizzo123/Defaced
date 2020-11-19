31860b7057e0b7ba05dc3354fbc2ab15fa553342
RenDeveloper <duncansrendall@gmail.com>
2020-11-19 11:08:21 +0000

    initial

---
+++ Assets/Scripts/Parallax/ParallaxBackground.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    private float length, startpos, ypos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        ypos = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+        float ydist = (cam.transform.position.y * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, ypos + ydist, transform.position.z);
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+    }
+}
